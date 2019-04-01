76c141364a282aa50ceb1a9c847b9710054b437c
Mike Wong <wongmdesign@gmail.com>
2019-04-01 17:31:08 -0400

    consumer added for color button

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import UserCreate from './UserCreate'
 import LanguageContext from '../contexts/LanguageContext'
-
+import ColorContext from '../contexts/ColorContext'
 export default class App extends Component {
   state = {
     language: 'english'
@@ -24,9 +24,11 @@ export default class App extends Component {
             onClick={() => this.onLanguageChange('dutch')}
           />
         </div>
-        <LanguageContext.Provider value={this.state.language}>
-          <UserCreate />
-        </LanguageContext.Provider>
+        <ColorContext.Provider value='red'>
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     )
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
 import LanguageContext from '../contexts/LanguageContext'
+import ColorContext from '../contexts/ColorContext'
 export default class Button extends Component {
   renderSubmit (value) {
     return value === 'english' ? 'Submit' : 'Voorleggen'
   }
-  render () {
+
+  renderButton (color) {
     return (
-      <button className='ui button primary'>
+      <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
           {value => this.renderSubmit(value)}
         </LanguageContext.Consumer>
       </button>
+    )
+  }
+
+  render () {
+    // consumer is good where we are consuming multiple context values
+    // context is good where we are doing just one value
+    return (
+      <ColorContext.Consumer>
+        {color => this.renderButton(color)}
+      </ColorContext.Consumer>
     )
   }
 }
