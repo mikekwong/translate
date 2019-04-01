dcf277119bde8ca0ed279528e75eacf1e476b902
Mike Wong <wongmdesign@gmail.com>
2019-04-01 16:35:19 -0400

    using consumer approach instead of context

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import LanguageContext from '../contexts/LanguageContext'
 export default class Button extends Component {
-  static contextType = LanguageContext
-
+  renderSubmit (value) {
+    return value === 'english' ? 'Submit' : 'Voorleggen'
+  }
   render () {
-    const text = this.context === 'english' ? 'Submit' : 'Voorleggen'
-    return <button className='ui button primary'>{text}</button>
+    return (
+      <button className='ui button primary'>
+        <LanguageContext.Consumer>
+          {value => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    )
   }
 }
 
